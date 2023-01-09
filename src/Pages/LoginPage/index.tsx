27b4ac9ea1666b9d7b067eba9d762d2163e73110
Lucas Mires <lucasmires10@gmail.com>
2023-01-09 18:43:12 -0300

    fix: folders allignment

---
+++ src/Pages/LoginPage/index.tsx
@@ -0,0 +1,14 @@
+import { LoginForm } from "./components/LoginForm"; 
+import { StyledMain } from "./style";
+
+export const LoginPage = () => {
+  return (
+    <StyledMain>
+      /* Will be the component with the Company's about. */
+      
+      <section>
+        <LoginForm />
+      </section>
+    </StyledMain>
+  );
+};
