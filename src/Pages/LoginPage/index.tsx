78f933db400a38ca4104ce5a72d4c1b9b0d9d5db
Lucas Mires <lucasmires10@gmail.com>
2023-01-10 09:43:11 -0300

    fix: main content

--- src/Pages/LoginPage/index.tsx
+++ src/Pages/LoginPage/index.tsx
@@ -5,6 +5,7 @@ export const LoginPage = () => {
   return (
     <StyledMain>
       /* Will be the component with the Company's about. */
+      
       <section>
         <LoginForm />
       </section>
