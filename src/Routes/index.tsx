d7f838a1a4b75fcfd26e92127da175a5e294fdd9
MuriloInouye <muriloysantos@hotmail.com>
2023-01-10 20:53:07 -0300

    add: requests

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -9,13 +9,25 @@ import { RegisterPage } from "../Pages/RegisterPage";
 export const MainRoutes = () => {
   return (
     <Routes>
-
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      <Route path="/" element={<MainHeader />}>          
-            <Route path="/dashboard" element={<ExchangeProvider><WalletProvider>Componentquevaisercriado</WalletProvider></ExchangeProvider>  } />
-            <Route path="/wallet" element={<ExchangeProvider><WalletProvider>Componentquevaisercriado</WalletProvider></ExchangeProvider>  } />
-       
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<MainHeader />}>
+        <Route
+          path="/dashboard"
+          element={
+            <ExchangeProvider>
+              <WalletProvider>Componentquevaisercriado</WalletProvider>
+            </ExchangeProvider>
+          }
+        />
+        <Route
+          path="/wallet"
+          element={
+            <ExchangeProvider>
+              <WalletProvider>Componentquevaisercriado</WalletProvider>
+            </ExchangeProvider>
+          }
+        />
       </Route>
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
