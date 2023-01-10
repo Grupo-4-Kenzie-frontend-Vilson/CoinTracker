1472f950617205b8950f1a390949f3d14be2cdb8
Carlos Maximiliano <maxi.krbr@gmail.com>
2023-01-10 09:56:12 -0300

    feat: header for dashboard

---
+++ src/Components/MainHeader/index.tsx
@@ -0,0 +1,27 @@
+import { AnimationLogo } from "../AnimationLogo"
+import searchIcon from "../../assets/search.svg"
+import walletIcon from "../../assets/wallet.svg"
+import logoutIcon from "../../assets/logout.svg"
+import { DivCenterContent, StyledMainHeader } from "./style"
+import { useMediaQuery } from 'react-responsive'
+
+export const MainHeader = () => {
+    const isBigScreen: boolean = useMediaQuery({query: '(min-width: 769px)'})
+
+    return (
+        <StyledMainHeader>
+            <DivCenterContent>
+                {
+                    isBigScreen === true && 
+                    <AnimationLogo height={120} width={80} />
+                }
+                <h1>CoinTracker</h1>
+            </DivCenterContent>
+            <DivCenterContent>
+                <img src={searchIcon} alt="search" />
+                <img src={walletIcon} alt="search" />
+                <img src={logoutIcon} alt="search" />
+            </DivCenterContent>
+        </StyledMainHeader>
+    )
+}
---
+++ src/Components/MainHeader/style.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledMainHeader = styled.header`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+background: #D9D9D9;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 0px 0px 10px 10px;
+
+@media only screen and (max-width: 768px){
+    padding: 10px 16px;
+}
+
+@media only screen and (min-width: 769px){
+    padding: 20px 52px;
+}
+`
+
+export const DivCenterContent = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+gap: 8px;
+`
