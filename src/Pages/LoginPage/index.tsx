8c1aacfb1c6a0edc03c7056a6801224e5319ba10
Doincode <yuridoin@gmail.com>
2023-01-10 16:07:58 -0400

    fix: contexts

--- src/Pages/LoginPage/index.tsx
+++ src/Pages/LoginPage/index.tsx
@@ -1,15 +1,17 @@
 
-import { LoginForm } from "./components/LoginForm"; 
+import { UserProvider } from "../../Contexts/user-context";
+import { LoginForm } from "./LoginForm"; 
 import { StyledMain, StyledSection } from "./style";
 
 
 export const LoginPage = () => {
   return (
-    <StyledMain>
-      /* Will be the component with the Company's about. */
-      <StyledSection>
-        <LoginForm />
-      </StyledSection>
-    </StyledMain>
+    <UserProvider>
+      <StyledMain>
+        <StyledSection>
+          <LoginForm />
+        </StyledSection>
+      </StyledMain>
+    </UserProvider>
   );
 };
