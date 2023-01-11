329f05b01b46bd5303a35f1ee4f892364ade301f
MuriloInouye <muriloysantos@hotmail.com>
2023-01-11 10:05:46 -0300

    add: logout function

--- src/Components/MainHeader/index.tsx
+++ src/Components/MainHeader/index.tsx
@@ -1,31 +1,31 @@
-import { AnimationLogo } from "../AnimationLogo"
-import searchIcon from "../../assets/search.svg"
-import walletIcon from "../../assets/wallet.svg"
-import logoutIcon from "../../assets/logout.svg"
-import { DivCenterContent, StyledMainHeader } from "./style"
-import { useMediaQuery } from 'react-responsive'
-import { Outlet } from "react-router-dom"
+import { AnimationLogo } from "../AnimationLogo";
+import searchIcon from "../../assets/search.svg";
+import walletIcon from "../../assets/wallet.svg";
+import logoutIcon from "../../assets/logout.svg";
+import { DivCenterContent, StyledMainHeader } from "./style";
+import { useMediaQuery } from "react-responsive";
+import { Outlet } from "react-router-dom";
+import { WalletContext } from "../../Contexts/wallet-context";
+import { useContext } from "react";
 
 export const MainHeader = () => {
-    const isBigScreen: boolean = useMediaQuery({query: '(min-width: 769px)'})
+  const isBigScreen: boolean = useMediaQuery({ query: "(min-width: 769px)" });
+  const { logout } = useContext(WalletContext);
 
-    return (
-        <>
-            <StyledMainHeader>
-                <DivCenterContent>
-                    {
-                        isBigScreen === true && 
-                        <AnimationLogo height={120} width={80} />
-                    }
-                    <h1>CoinTracker</h1>
-                </DivCenterContent>
-                <DivCenterContent>
-                    <img src={searchIcon} alt="search" />
-                    <img src={walletIcon} alt="search" />
-                    <img src={logoutIcon} alt="search" />
-                </DivCenterContent>
-            </StyledMainHeader>
-            <Outlet />
-        </>
-    )
-}
+  return (
+    <>
+      <StyledMainHeader>
+        <DivCenterContent>
+          {isBigScreen === true && <AnimationLogo height={120} width={80} />}
+          <h1>CoinTracker</h1>
+        </DivCenterContent>
+        <DivCenterContent>
+          <img src={searchIcon} alt="search" />
+          <img src={walletIcon} alt="search" />
+          <img src={logoutIcon} alt="search" onClick={() => logout()} />
+        </DivCenterContent>
+      </StyledMainHeader>
+      <Outlet />
+    </>
+  );
+};
--- src/Contexts/exchange-context/index.tsx
+++ src/Contexts/exchange-context/index.tsx
@@ -7,9 +7,9 @@ interface iExchangeContextProps {
 }
 
 interface iExchangeContext {
-  allCoins: [] | iCoin[]; 
-  favoriteCoins: iCoin[]; 
-  setFavoriteCoins: React.Dispatch<SetStateAction<[] | iCoin[]>>; 
+  allCoins: [] | iCoin[];
+  favoriteCoins: iCoin[];
+  setFavoriteCoins: React.Dispatch<SetStateAction<[] | iCoin[]>>;
   dollarPrice: number | null;
 }
 
@@ -21,24 +21,21 @@ interface iCoin {
   symbol: string;
 }
 
-
 export const ExchangeContext = createContext({} as iExchangeContext);
 
 export const ExchangeProvider = ({ children }: iExchangeContextProps) => {
-
-  const [allCoins, setAllCoins]   = useState<[] | iCoin[]>([])
-  const [favoriteCoins, setFavoriteCoins]   = useState<[] | iCoin[]>([])
-  const [dollarPrice, setDollarPrice]   = useState<number | null>(null)
-  
+  const [allCoins, setAllCoins] = useState<[] | iCoin[]>([]);
+  const [favoriteCoins, setFavoriteCoins] = useState<[] | iCoin[]>([]);
+  const [dollarPrice, setDollarPrice] = useState<number | null>(null);
 
   async function fetchAllCoins() {
     try {
       const fetch = await coinRankingApi.get(`/coins?scopeLimit=20`, {
         headers: {
-          'x-access-token': 'coinranking4b49976245587b192ea41f41cb9c2d69147a79a9bb66f6d4'       
+          "x-access-token":
+            "coinranking4b49976245587b192ea41f41cb9c2d69147a79a9bb66f6d4",
         },
       });
-      console.log(fetch.data.data.coins);
       setAllCoins(fetch.data.data.coins);
     } catch (err) {
       console.log(err);
@@ -48,15 +45,20 @@ export const ExchangeProvider = ({ children }: iExchangeContextProps) => {
   async function fetchDollarPricing() {
     try {
       const fetch = await awesomeApi.get(``);
-      console.log(fetch.data.USDBRL.ask);
       setDollarPrice(fetch.data.USDBRL.ask);
     } catch (err) {
       console.log(err);
     }
   }
 
-  fetchAllCoins()
-  fetchDollarPricing()
-  
-  return <ExchangeContext.Provider value={{allCoins, favoriteCoins, setFavoriteCoins, dollarPrice}}>{children}</ExchangeContext.Provider>;
+  fetchAllCoins();
+  fetchDollarPricing();
+
+  return (
+    <ExchangeContext.Provider
+      value={{ allCoins, favoriteCoins, setFavoriteCoins, dollarPrice }}
+    >
+      {children}
+    </ExchangeContext.Provider>
+  );
 };
